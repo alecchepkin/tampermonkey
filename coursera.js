b3bd0865491f97ea4c599fffcf6e0a38dd3ee58b
olegre <olegre@gmail.com>
2020-01-30 19:04:23 +0200

    reenable scrool for next page

--- coursera.js
+++ coursera.js
@@ -1,31 +1,31 @@
 // ==UserScript==
 // @name         Coursera transcript scroll
 // @namespace    https://github.com/olegre/tampermonkey/blob/master/coursera.js
-// @version      1.2.2
+// @version      1.2.3
 // @description  make a scroll for transcript and make resizable the video player
 // @author       You
 // @match        https://www.coursera.org/learn/*
 // @grant        none
 // ==/UserScript==
 
-if(document.body && document.domain == 'www.coursera.org') {
+if (document.body && document.domain == 'www.coursera.org') {
     var scrollIntervalCoursera = setInterval(transcScrollEnable, 1000);
 }
 
-function transcScrollEnable(){
+function transcScrollEnable() {
+    if (document.getElementsByClassName("rc-VideoTranscriptToolbar")[0].style.display == "none") {
+        return
+    }
     const tr = document.getElementsByClassName("rc-InteractiveTranscript");
     tr[0].style.height = "300px";
     tr[0].style.overflow = "scroll";
-    document.getElementsByClassName("rc-VideoTranscriptToolbar")[0].style.display="none";
-    document.getElementsByClassName("rc-VideoItemWithHighlighting")[0].style.maxWidth="none";
-    document.getElementsByClassName("rc-InteractiveTranscript")[0].style.maxWidth="none";
-    document.getElementsByClassName("rc-VideoToolbar")[0].style.maxWidth="none";
-    document.getElementsByClassName("rc-Transcript")[0].style.maxWidth="none";
-    document.getElementsByClassName("video-name")[0].style.display="none";
-    //console.log("enable transcript scroll");
-    if(document.getElementsByClassName("rc-VideoTranscriptToolbar")[0].style.display=="none"){
-        clearInterval(scrollIntervalCoursera)
-    }
+    document.getElementsByClassName("rc-VideoTranscriptToolbar")[0].style.display = "none";
+    document.getElementsByClassName("rc-VideoItemWithHighlighting")[0].style.maxWidth = "none";
+    document.getElementsByClassName("rc-InteractiveTranscript")[0].style.maxWidth = "none";
+    document.getElementsByClassName("rc-VideoToolbar")[0].style.maxWidth = "none";
+    document.getElementsByClassName("rc-Transcript")[0].style.maxWidth = "none";
+    document.getElementsByClassName("video-name")[0].style.display = "none";
+
 }
 
 transcScrollEnable();
